46a2cdc11271a5d53fde7148f53c0b1096db6cb1
PedroDev12 <pedroluisperez33@gmail.com>
2025-12-23 16:18:44 -0400

    cambios

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        @vite('resources/css/app.css')
+
+    
+    </head>
+    <body>
+
+        <nav>
+            <a href="/">Principal</a>
+            <a href="/nosotros">Nosotros</a>
+            <a href="tienda">Tienda</a>
+        </nav>
+
+        <h1>@yield('titulo')</h1>
+
+        <hr>
+
+        @yield('contenido')
+          
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    return view('layouts/app');
+});
+
+Route::get('/', function () {
+    return view('principal');
 });
